90f083d42985636ba588cae477b1aa78a60d7c63
isserterrus <david@switchsoft.fr>
2020-01-17 18:53:02 +0100

    Update config.php

--- BASE_WWW/application/config/config.php
+++ BASE_WWW/application/config/config.php
@@ -27,7 +27,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = '';
 
 // Version David
-$config['base_url'] = 'http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/MPL_2020/';
+//$config['base_url'] = 'http://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].'/MPL_2020/';
 
 /*
 |--------------------------------------------------------------------------
